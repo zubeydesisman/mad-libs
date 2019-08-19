ff819a740a75b5bc8eb97955edcc402b6815dad1
Student <example@example.com>
2019-08-19 14:44:20 -0700

    updating index.html and scripts.js using loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+  $("#formOnes").submit(function(event) {
+    var formOnes = ["person1","person2","animal","exclamation","verb","noun"];
+    formOnes.forEach(function(formOne) {
+      var userInput = $("input#"+formOne).val();
+      $("."+formOne).text(userInput);
+    });
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+
+
+  //  var person1Input = $("input#person1").val();
+  //  var person2Input = $("input#person2").val();
+  //  var animalInput= $("input#animal").val();
+  //  var exclamationInput = $("input#exclamation").val();
+  //  var verbInput = $("input#verb").val();
+  //  var nounInput = $("input#noun").val();
+
+  //  $(".person1").text(person1Input);
+  //  $(".person2").text(person2Input);
+  //  $(".animal").text(animalInput);
+  //  $(".exclamation").text(exclamationInput);
+  //  $(".verb").text(verbInput);
+    //$(".noun").text(nounInput);
 
     $("#story").show();
 
